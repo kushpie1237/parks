8f794acf140d8631c83f15ad70b7ecbd7d9938dd
kushpie1237 <137706946+kushpie1237@users.noreply.github.com>
2023-06-26 21:31:24 +0300

    Update MatrixFrobeniusNormTask.java

--- src/MatrixFrobeniusNormTask.java
+++ src/MatrixFrobeniusNormTask.java
@@ -3,48 +3,22 @@ import java.util.Random;
 
 public class MatrixFrobeniusNormTask implements AM {
 
-private String sizeStr;
+    public void run(AMInfo info) {
+        int[] size = (int[]) info.parent.readObject();
+        int[][] a = (int[][]) info.parent.readObject();
+        double norm = 0.0;
 
-public MatrixFrobeniusNormTask(String sizeStr) {
-this.sizeStr = sizeStr;
-}
+        // Обчислення норми Фробеніуса
+        for (int i = 0; i < size[0]; i++) {
+            for (int j = 0; j < size[1]; j++) {
+                norm += Math.pow(a[i][j], 2);
+            }
+        }
 
-public void run(AMInfo info) {
-int[] size = deserializeSize(sizeStr);
-int[][] a = new int[size[0]][size[1]];
-double norm = 0.0;
+        norm = Math.sqrt(norm);
 
-
-java.util.Random rnd = new java.util.Random();
-
-// Заполнение матрицы случайными числами
-for (int i = 0; i < size[0]; i++) {
-    for (int j = 0; j < size[1]; j++) {
-        a[i][j] = rnd.nextInt(10);
-        norm += Math.pow(a[i][j], 2);
+        // Повернення результату
+        info.parent.write(Double.toString(norm));
     }
-}
 
-norm = Math.sqrt(norm);
-
-// Обчислення норми Фробеніуса
-for (int i = 0; i < size[0]; i++) {
-    for (int j = 0; j < size[1]; j++) {
-        norm += Math.pow(a[i][j], 2);
-    }
-}
-
-norm = Math.sqrt(norm);
-
-// Повернення результату
-info.send(Double.toString(norm));
-}
-
-private int[] deserializeSize(String sizeStr) {
-String[] sizeArr = sizeStr.split(",");
-int[] size = new int[2];
-size[0] = Integer.parseInt(sizeArr[0]);
-size[1] = Integer.parseInt(sizeArr[1]);
-return size;
-}
 }
